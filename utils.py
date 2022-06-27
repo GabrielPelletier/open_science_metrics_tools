104afd6c9f21ec9ce7b33b6b52dcc4bcb6534312
GabrielPelletier <gabriel.pelletier@mcgill.ca>
2022-06-27 15:49:37 -0400

    nice html formatting for master list of tracker

--- utils.py
+++ utils.py
@@ -66,19 +66,31 @@ def create_html_file(csv_file):
     # Create an "entry" for each row in the CSV
     for line in data[1:]:
         row = line.split(",")
-        # Publication Title
-        my_html_file += "<h3>" + row[1] + "</h3>\n"
-        #my_html_file += "</br>\n"
-        # Publication Date
-        my_html_file += "<p> Publication Date: " + row[5] + "</p>\n"
-        # Open Access Status
+        # Define text and formatting based on Open Access Status
         if row[7] == 'closed':
-            oa_text = "<p> This publication is not available in Open Access :( </p>\n"
+            title_text = '<h3 style="color:silver">' + row[1] + '</h3>\n'
+            date_text = '<p style="color:silver">&emsp;Publication Date: ' + row[5] + '</p>\n'
+            oa_text = '<p style="color:silver">&emsp;This publication is not available in Open Access : ( </p>\n'
         elif row[7] == 'gold':
-            oa_text = '<p style="color:darkorange"><strong>This publication is Gold Open Access!</strong><p>\n<p><a href=' + row[9] + ' target="_blank">Access it freely here.</a></p>\n'
-        else:
-            oa_text = "<p> else </p>\n"
+            title_text = '<h3>' + row[1] + '</h3>\n'
+            date_text = '<p>&emsp;Publication Date: ' + row[5] + '</p>\n'
+            oa_text = '<p><strong>&emsp;This publication is available in Open Access! <span style="color:darkorange">(Gold OA)</span></strong></p>\n<p>&emsp;<a href=' + \
+                      row[9] + ' target="_blank">Access it freely here.</a></p>\n'
+        elif row[7] == 'hybrid':
+            title_text = '<h3>' + row[1] + '</h3>\n'
+            date_text = '<p>&emsp;Publication Date: ' + row[5] + '</p>\n'
+            oa_text = '<p><strong>&emsp;This publication is available in Open Access! <span style="color:grey">(Hybrid OA)</span></strong></p>\n<p>&emsp;<a href=' + \
+                      row[9] + ' target="_blank">Access it freely here.</a></p>\n'
+        elif row[7] == 'green':
+            title_text = '<h3>' + row[1] + '</h3>\n'
+            date_text = '<p>&emsp;Publication Date: ' + row[5] + '</p>\n'
+            oa_text = '<p><strong>&emsp;This publication is available in Open Access! <span style="color:green">(Green OA)</span></strong></p>\n<p>&emsp;<a href=' + \
+                      row[9] + ' target="_blank">Access it freely here.</a></p>\n'
+        # Write to file
+        my_html_file += title_text
+        my_html_file += date_text
         my_html_file += oa_text
+
         my_html_file += "</br>\n"
 
     my_html_file += "<body>\n"
