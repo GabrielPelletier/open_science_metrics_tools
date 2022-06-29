ac9ad3b154cf452a00b042cbf57eafa13a8d650b
GabrielPelletier <gabriel.pelletier@mcgill.ca>
2022-06-29 15:45:54 -0400

    integrated markdown files writer function to OA tracker

--- scripts/utils.py
+++ scripts/utils.py
@@ -99,3 +99,53 @@ def create_html_file(csv_file):
     fileout.writelines(my_html_file)
     fileout.close()
     filein.close()
+
+
+def create_md_files(csv_file, out_dir):
+    # Parses the CSV list containing many publications, and creates one markdown doc (.md)
+    # for each of them, in preparation for the website display in a neat timeline.
+
+    # input file
+    filein = open(csv_file, "r")
+    data = filein.readlines()
+
+    pub_num = 0
+
+    for line in data[1:]:
+        row = line.split(",")
+        pub_num += 1
+        # create separate output md file for each publication
+        md_file_name = out_dir + 'pub_' + str(pub_num) + '.md'
+        fileout = open(md_file_name, "w")
+        date_text = datetime.strptime(row[5], '%m/%d/%Y').strftime('%Y-%m-%d') + " 00:00:00 -0700"
+        oa_logo_link = '"https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Open_Access_logo_PLoS_transparent.svg/800px-Open_Access_logo_PLoS_transparent.svg.png"'
+        # Define text and formatting based on Open Access Status
+        if row[7] == 'closed':
+            oa_text = 'This article is not available in Open Access\n\n'
+        elif row[7] == 'gold':
+            oa_text = '<img src=' + oa_logo_link + ' alt="drawing" width="50" align="left"/> &nbsp;&nbsp;&nbsp;This publication is available in **Open Access**! (Gold OA)\n\n' + \
+                      '&nbsp;&nbsp;&nbsp;[Access it freely here](' + row[9] + ')\n'
+        elif row[7] == 'hybrid':
+            oa_text = '<img src=' + oa_logo_link + ' alt="drawing" width="50" align="left"/> &nbsp;&nbsp;&nbsp;This publication is available in **Open Access**! (Hybrid OA)\n\n' + \
+                      '&nbsp;&nbsp;&nbsp;[Access it freely here](' + row[9] + ')\n'
+        elif row[7] == 'green':
+            oa_text = '<img src=' + oa_logo_link + ' alt="drawing" width="50" align="left"/> &nbsp;&nbsp;&nbsp;This publication is available in **Open Access**! (Green OA)\n\n' + \
+                      '&nbsp;&nbsp;&nbsp;[Access it freely here](' + row[9] + ')\n'
+        elif row[7] == 'bronze':
+            oa_text = '<img src=' + oa_logo_link + ' alt="drawing" width="50" align="left"/> &nbsp;&nbsp;&nbsp;This publication is available in **Open Access**! (Bronze OA)\n\n' + \
+                      '&nbsp;&nbsp;&nbsp;[Access it freely here](' + row[9] + ')\n'
+
+        my_md_file = "---\n"
+        my_md_file += 'title: ' + '"' + row[1] + '"' + '\n'
+        my_md_file += "date: " + date_text + "\n"
+        my_md_file += "enddate:\n"
+        my_md_file += "---\n"
+        my_md_file += "\n"
+        my_md_file += 'Published in: *' + row[2] + '*\n\n'
+        my_md_file += 'DOI: ' + row[3] + '\n\n'
+        my_md_file += oa_text + "\n"
+
+        fileout.writelines(my_md_file)
+        fileout.close()
+
+    filein.close()
